fe0b2a1f3b535a897053287a7858a652ea5d48a6
Pratik Asatkar <praikasatkar11@gmail.com>
2022-10-05 23:02:50 +0530

    taking the input amount

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def deposit():
+    while True:
+        amount = input("what amount would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("amount must be greater than zero.")
+        else:
+            print("please  enter a number.")
